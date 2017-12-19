2de6ab95e44052f5ed994d9acc6335883a8a7275
weiguang <chen2621978@gmail.com>
2017-12-19 18:38:22 +0800

    增加了循环跑不动函数和维度

--- myRun.m
+++ myRun.m
@@ -5,11 +5,12 @@ d=[5,15,30];
 % d=[2,5,15,30];
 % d=[2];
  [m n] = size(d);
+ func = 'Ackley'
 for i = 1:n  
     con  = 1;
     while(con >0)
          clc;
-         Rundeopt(d(i));
+         runTest(func,d(i));
          con = input('请输入重跑,回车继续下一维:');
     end
 end
