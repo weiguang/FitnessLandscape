85a701b559621a2e24783001a15783d5ef0043c7
weiguang <chen2621978@gmail.com>
2018-01-21 15:10:15 +0800

    修改了目录结构

---
+++ DEInterface.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+addpath(genpath(pwd));
+% d=[5,15,30];
+ d=[2,5,15,30];
+% d=[2];
+ [m n] = size(d);
+ func_num = 1;
+for i = 1:n  
+    con  = 1;
+    while(con >0)
+         clc;
+         runTestCEC(func_num,d(i));
+         con = input('请输入重跑,回车继续下一维:');
+    end
+end
