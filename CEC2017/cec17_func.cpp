e6751929ac7f5a09696a72e84d64dac5d8a95514
weiguang <chen2621978@gmail.com>
2018-01-22 20:50:52 +0800

    修改了abs为 fabs

--- CEC2017/cec17_func.cpp
+++ CEC2017/cec17_func.cpp
@@ -434,7 +434,7 @@ void sum_diff_pow_func (double *x, double *f, int nx, double *Os,double *Mr, int
 	for (i=0; i<nx; i++)
 	{
 		double xi = z[i];
-		double newv = pow((abs(xi)),(i+1));
+		double newv = pow((fabs(xi)),(i+1));
 		sum = sum + newv;
 	}
 	
