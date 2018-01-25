f830313e782a75fc7f81eafe50b5d493b2c4beac
weiguang <chen2621978@gmail.com>
2018-01-25 15:35:17 +0800

    增加了天添加参数的时候顺便编译cpp文件

--- AddWorkPath.m
+++ AddWorkPath.m
@@ -3,12 +3,12 @@ function AddWorkPath()
 addpath(pwd);
 addpath(genpath([pwd '\DE']));
 addpath(genpath([pwd '\CEC2017']));
-addpath(genpath([pwd '\NewFunction']));
-addpath(genpath([pwd '\OldFunctions']));
+% addpath(genpath([pwd '\NewFunction']));
+% addpath(genpath([pwd '\OldFunctions']));
 addpath(genpath([pwd '\RunCEC']));
 
-cd CEC2017
-mex cec17_func.cpp
-cd ..
+ cd CEC2017
+ mex cec17_func.cpp
+
 
 end
