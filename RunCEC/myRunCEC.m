2bc14ff32ec78ee739320c3834b34f5a42aac314
weiguang <chen2621978@gmail.com>
2018-01-30 15:39:15 +0800

    修改了检查函数

--- RunCEC/myRunCEC.m
+++ RunCEC/myRunCEC.m
@@ -5,9 +5,10 @@ clear;
 %d=[2,5,15,30];
 %func_num = 1;
 % d=[10,30,50,100]; % cec2017
+
 d=[2,10];
 [m n] = size(d);
-for func_num = 1:11
+for func_num = 1:30
     for i = 1:n
         con  = 1;
         while(con >0)
@@ -28,24 +29,33 @@ re = 0;
 if func_num == 2
     re = 3;
     disp("This function (F2) has been deleted");
-elseif func_num < 11 && D == 2  %前10个测试函数可以是2维
-    re = 0;
+elseif  func_num < 11 && D == 2 %前10个测试函数可以是2维
+    re  = 0
+elseif func_num > 10 &&  func_num <21 && D == 2  %11-20个测试函数不可以是2维
+    disp("Test functions 11-20  are only defined for D=10,30,50,100.");
+    re = -1;
 else
-    dataFile = sprintf('shift_data_%d.txt',func_num);
+    dataFile = sprintf('M_%d_D%d.txt',func_num,D);
     if exist(dataFile,'file')==0
         %     error(['no data file:' dataFile]);
         disp("error:");
         disp(['no data file:' dataFile]);
         re = 1;
     end
+    dataFile = sprintf('shift_data_%d.txt',func_num);
+    if exist(dataFile,'file')==0
+        %     error(['no data file:' dataFile]);
+        disp("error:");
+        disp(['no data file:' dataFile]);
+        re = 2;
+    end
     dataFile = sprintf('input_data/shuffle_data_%d_D%d.txt',func_num,D);
     if exist(dataFile,'file')==0
         %    error(['no data file:' dataFile]);
         disp("error:");
         disp(['no data file:' dataFile]);
-        re = 2;
+        re = 3;
     end
 end
-
 
 end
