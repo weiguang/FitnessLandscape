8fede17d3853627b28c34f1e4d9c4ba41fdd4cc4
weiguang <chen2621978@gmail.com>
2018-02-01 10:30:44 +0800

    修改函数维度判断，避免某些维度无法计算导致matlab崩溃

--- CEC2017/testCPP.m
+++ CEC2017/testCPP.m
@@ -1,7 +1,8 @@
 function [fitness] = testCPP()
-func_num = 1;
+func_num = 10;
+D = 2;
 addpath(genpath(pwd));
 fhd=str2func('cec17_func');
-x=ones(1,10);
+x=randi([-100,100],1,D);
 fitness = feval(fhd,x',func_num);
 end
